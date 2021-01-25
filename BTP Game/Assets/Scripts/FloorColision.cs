d9e7afaa6cb6a284634c71c306bbaaee72e03f4a
scriche <59908731+scriche@users.noreply.github.com>
2021-01-24 21:34:51 -0800

    Added Wall jump, Bounce Pad, Better movement

--- BTP Game/Assets/Scripts/FloorColision.cs
+++ BTP Game/Assets/Scripts/FloorColision.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FloorColision : MonoBehaviour
 {
     public bool onfloor = false;
+    public bool onwall = false;
+    public int wallcount = 0;
     public int colcount = 0;
 
     // Start is called before the first frame update
@@ -15,12 +17,22 @@ public class FloorColision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        colcount++; 
+        if (other.gameObject.tag != "wall")
+        {
+            colcount++; 
+        } else {
+            wallcount++;
+        }
     }
     
     private void OnTriggerExit2D(Collider2D other)
     {
-        colcount--;    
+        if (other.gameObject.tag != "wall")
+        {
+            colcount--;
+        } else {
+            wallcount--;
+        }
     }
 
     void Update()
@@ -30,6 +42,13 @@ public class FloorColision : MonoBehaviour
             onfloor = true;
         } else {
             onfloor = false;
+        }
+
+        if (wallcount > 0)
+        {
+            onwall = true;
+        } else {
+            onwall = false;
         }
     }
 }
--- BTP Game/Assets/Scripts/PlayerMovement.cs
+++ BTP Game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float yo;
+    public int walljumpcount;
+    public int walljumps = 1;
+    public float wallspeed = -1f;
     public float moveSpeed = 5f;
     public float jumpforce = 5f;
     public float fallmultiplier = 2.5f;
@@ -16,20 +19,43 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Seting a reference to this gameObject's Rigidbody and Transform
         rb = gameObject.GetComponent<Rigidbody2D>();
         tf = gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
+        //setting variables back to there deafults every frame
+        //Which is overwriten by setting the variables again benith 
+        //rb.velocity = (new Vector2(0, rb.velocity.y));
+        rb.gravityScale = 4;
+
+        //Debuging Variable
         yo = rb.velocity.y;
 
+        //When pressing left or right set varible x to -1 or 1
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector2 dir = new Vector2(x, y);
+        //Creating a Vector2 to refernce when wanting to check if any buttons where pressed
 
-        jump();
+        
         walk(dir);
+        jump();
+        //Called when colliding with the floor
+        if (floorColision.onfloor)
+        {
+            walljumpcount = 0;
+        }
+
+        //called when colliding with the wall and not on the floor and the character is falling
+        if (floorColision.onwall && !floorColision.onfloor && x != 0 && rb.velocity.y < 0)
+        {
+            //This add the wall slide and slow decent
+            rb.gravityScale = 0;
+            rb.velocity = (new Vector2(rb.velocity.x, wallspeed));
+        }
     }
 
     void Shoot()
@@ -42,17 +68,27 @@ public class PlayerMovement : MonoBehaviour
 
     void walk(Vector2 dir)
     {
+        //set the character velocity depefing the the value of the x varible times the speed
         rb.velocity = (new Vector2(dir.x * moveSpeed, rb.velocity.y));
     }
 
     void jump()
     {
+        //Making sure you can only jump when on the ground or on the side of
+        //a wall with a wall jump remaining
         if (Input.GetButtonDown ("Jump") && floorColision.onfloor)
         {
             rb.velocity += Vector2.up * jumpforce;
+        } else
+        if (Input.GetButtonDown ("Jump") && floorColision.onwall && walljumpcount < walljumps)
+        {
+            rb.velocity = (new Vector2((rb.velocity.x * -1) * 2, 1 * jumpforce));
+            walljumpcount++;
+            Debug.Log("Walljmup");
         }
 
-        if (rb.velocity.y < 10) 
+        //adds holding jump to jump longer and quicker decent than acent
+        if (rb.velocity.y < 0 && !floorColision.onwall) 
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallmultiplier - 1) * Time.deltaTime;
         } else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
