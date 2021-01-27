e73abb485b8c8907036093f1e7d0a794f10a67d5
Benjamin Reedel <benjamin_reedel@sfu.ca>
2021-01-26 22:00:08 -0800

    Static Script Fix

--- BTP Game/Assets/Scripts/PlayerStats.cs
+++ BTP Game/Assets/Scripts/PlayerStats.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 using System.Collections;
 
-public static class PlayerStats : MonoBehaviour {
+public static class PlayerStats {
 
-	public static int Energy;
+	public static int Energy = 100;
 	public static int Round = 1;
-	public static int startEnergy = 100;
 
-	void Start ()
-	{
-		Energy = startEnergy;
+	public static int Rounds {
+		get {
+			return Round;
+		}
+		set {
+			Round = value;
+		}
+	}
+
+	public static int Energys {
+		get {
+			return Energy;
+		}
+		set {
+			Energy = value;
+		}
 	}
 
 }
